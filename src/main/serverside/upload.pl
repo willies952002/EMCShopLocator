fe1a4193cbb05d10a19227b745ac6046e56eb90c
Guntram Blohm <gbl@guntram.de>
2017-03-15 20:01:40 +0100

    new table format, more columns

--- src/main/serverside/upload.pl
+++ src/main/serverside/upload.pl
@@ -9,22 +9,26 @@ my $dbh=DBI->connect("DBI:mysql:database=emcmarket;host=localhost", "emcmarket",
 my $cgi=CGI->new;
 my $rows=0;
 if ($cgi->param("upload")) {
+	my $name=$cgi->param("name");
 	my $sthdel=$dbh->prepare(qq(
 		delete from signs
 		where server=? and x=? and y=? and z=?
 	));
 	my $sthins=$dbh->prepare(qq(
-		insert into signs (server, x, y, z, amount, buy, sell, owner, item, resnumber)
-		values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		insert into signs (server, x, y, z, amount, buy, sell, owner, item, resnumber, chooseposition, lastseen, uploader)
+		values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 	));
 	foreach my $row (split("\n", $cgi->param("upload"))) {
-		my ($server, $x, $y, $z, $amount, $buy, $sell, $owner, $item, $resnumber)=
+		my ($server, $x, $y, $z, $amount, $buy, $sell, $owner, $item, $resnumber, $choosepos, $lastseen)=
 			split(":", $row);
 		$sthdel->execute($server, $x, $y, $z);
-		$sthins->execute($server, $x, $y, $z, $amount, $buy, $sell, $owner, $item, $resnumber);
+		$sthins->execute($server, $x, $y, $z, $amount, $buy, $sell, $owner, $item, $resnumber, $choosepos, $lastseen, $name);
 		$rows++;
 	}
 	$dbh->commit;
+	open(F, ">>/tmp/uploaders.dat");
+	print F scalar localtime, "  ",  $rows ," uploaded by ", $cgi->param("name"), "\n";
+	close F;
 }
 
 print qq(Content-type: text/plain\n);
