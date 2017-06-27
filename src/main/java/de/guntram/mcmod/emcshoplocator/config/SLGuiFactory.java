8edf8f4de424e947f171aa7cca6a8245e35a92cd
Guntram Blohm <gbl@bso2001.com>
2017-06-27 10:51:40 +0200

    changes for forge 2375

--- src/main/java/de/guntram/mcmod/emcshoplocator/config/SLGuiFactory.java
+++ src/main/java/de/guntram/mcmod/emcshoplocator/config/SLGuiFactory.java
@@ -17,22 +17,12 @@ public class SLGuiFactory implements IModGuiFactory {
     }
     
     @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass() {
-        return SLGuiConfig.class;
-    }
-
-    @Override
     public GuiScreen createConfigGui(GuiScreen parentScreen) {
         return new SLGuiConfig(parentScreen);
     }
     
     @Override
     public Set<IModGuiFactory.RuntimeOptionCategoryElement> runtimeGuiCategories() {
-        return null;
-    }
-    
-    @Override
-    public IModGuiFactory.RuntimeOptionGuiHandler getHandlerFor(final IModGuiFactory.RuntimeOptionCategoryElement element) {
         return null;
     }
 }
